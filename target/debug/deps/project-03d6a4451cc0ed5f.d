b01a5300b19a1d39dbe0f063b9a494625085de79
balazs19 <s.balazs.bence@gmail.com>
2021-02-20 09:25:06 +0100

    first commit

---
+++ target/debug/deps/project-03d6a4451cc0ed5f.d
@@ -0,0 +1,5 @@
+/home/user/Documents/project_002/project/target/debug/deps/project-03d6a4451cc0ed5f: src/main.rs
+
+/home/user/Documents/project_002/project/target/debug/deps/project-03d6a4451cc0ed5f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/project.d
@@ -0,0 +1,1 @@
+/home/user/Documents/project_002/project/target/debug/project: /home/user/Documents/project_002/project/src/main.rs
